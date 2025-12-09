1fc1453cdad74b6896d5c9e8216ae66fa44adeb5
esat54 <esatsprx77@gmail.com>
2025-12-09 23:47:03 +0300

    Reset project structure and migrate to new architecture

--- src/routes/dictionaryRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const dictionaryController = require('../controllers/dictionaryController');
-
-router.get('/', dictionaryController.getDictionary);
-router.post('/', dictionaryController.postDictionary);
-
-module.exports = router;
