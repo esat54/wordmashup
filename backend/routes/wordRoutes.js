4956b90ee06f634d05d1bed2cb7fc67d3ac76b07
esat54 <esatsprx77@gmail.com>
2025-12-25 23:06:31 +0300

    Deletion and favorites routes added

--- backend/routes/wordRoutes.js
+++ backend/routes/wordRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { addWord } = require('../controllers/wordController');
+const { addWord, getWords, addtoFavorites, deleteWord } = require('../controllers/wordController');
 const { authenticate } = require('../middleware/authMiddleware');
 
-router.post('/', authenticate, addWord);
 
+router.post('/', authenticate, addWord);
+router.get('/', authenticate, getWords);
+router.post('/:wordId/favorite', authenticate, addtoFavorites);
+router.delete('/:wordId', authenticate, deleteWord);
 
 module.exports = router;
