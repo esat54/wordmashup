4e4fe1b9e7b18d4135c46a54daf064d4937e3d0f
esat54 <esatsprx77@gmail.com>
2025-12-27 13:06:22 +0300

    new functions added

--- backend/controllers/wordController.js
+++ backend/controllers/wordController.js
@@ -3,7 +3,6 @@ const Word = require('../models/wordModel');
 exports.addWord = async (req, res) => {
     try {
         console.log('Request body:', req.body);
-        console.log('User ID:', req.userId);
 
         const { text, translation, exampleSentence, sentenceTranslation, type } = req.body;
 
@@ -33,3 +32,48 @@ exports.addWord = async (req, res) => {
     }
 };
 
+
+exports.getWords = async (req, res) => {
+    try {
+        const words = await Word.find({ addedBy: req.userId });
+        res.status(200).json(words);
+    } catch (error) {
+        console.error('Kelime getirme hatası:', error);
+        res.status(500).json({ message: 'Sunucu hatası' });
+    }
+}
+
+
+exports.addtoFavorites = async (req, res) => {
+    try {
+        const wordId = req.params.wordId;
+        const word = await Word.findById(wordId);
+        if (!word) {
+            return res.status(404).json({ message: 'Kelime bulunamadı' });
+        }
+        word.favorite = !word.favorite;
+        await word.save();
+        res.status(200).json({ word });
+    } catch (error) {
+        console.error('Kelime getirme hatası:', error);
+    }
+}
+
+
+exports.deleteWord = async (req, res) => {
+    try {
+        const wordId = req.params.wordId;
+        const word = await Word.findByIdAndDelete(wordId);
+
+        if (!word) {
+            return res.status(404).json({ message: 'Kelime bulunamadı' });
+        }
+
+        return res.status(200).json({
+            message: 'Kelime başarıyla silindi'
+        });
+    } catch (error) {
+        console.error('Kelime silme hatası:', error);
+        return res.status(500).json({ message: 'Sunucu hatası' });
+    }
+};
